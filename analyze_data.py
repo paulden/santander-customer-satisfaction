4593330b4a3c6e44af7a906f1a736b85247846bd
paulden <paul.de-nonancourt@student.ecp.fr>
2017-11-26 21:01:43 +0100

    Clean data by removing constant and duplicate columns

--- analyze_data.py
+++ analyze_data.py
@@ -6,6 +6,13 @@ import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 
+from clean_data import *
+
 df = pd.read_csv('./data/train.csv')
 
+# CLEAN DATA
+df = remove_constant_columns(df)
+df = remove_duplicate_columns(df)
+
 train_df, test_df = train_test_split(df, test_size=0.2)
+combine = [train_df, test_df]
