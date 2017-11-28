69ce5efe5b9922463eeaaf7c8009807a78b17ffa
paulden <paul.de-nonancourt@student.ecp.fr>
2017-11-28 21:22:44 +0100

    Select most important features using ExtraTreesClassifier

--- analyze_data.py
+++ analyze_data.py
@@ -6,12 +6,13 @@ import seaborn as sns
 
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score as auc
+from sklearn.metrics import roc_auc_score
 from sklearn.metrics import accuracy_score
 
 from xgboost import XGBClassifier
 
 from clean_data import *
+from select_features import *
 
 
 df = pd.read_csv('./data/train.csv')
@@ -33,6 +34,12 @@ X_test = test_df.drop(['ID', 'TARGET'], axis=1)
 y_train = train_df['TARGET']
 y_test = test_df['TARGET']
 
+# Feature selection
+print("\nSelecting most important features to use using ExtraTreesClassifier")
+X_train, X_test = extra_trees(X_train, y_train, X_test)
+
+print(X_train.shape, X_test.shape)
+
 # GradientBoostingClassifier
 print("\nClassifying using GradientBoostingClassifier...")
 gb_classifier = GradientBoostingClassifier(random_state=1)
@@ -42,17 +49,16 @@ gb_predictions = gb_classifier.predict(X_test)
 
 print("Computing scores...")
 gb_accuracy_score = accuracy_score(y_test, gb_predictions)
-gb_auc_score = auc(y_test, gb_predictions)
-print("Accuracy score: %s - AUC score: %s\n" % (gb_accuracy_score, gb_auc_score))
+gb_auc_score = roc_auc_score(y_test, gb_predictions)
+print("AUC score: %s\n" % gb_auc_score)
 
 # XGBClassifier
 print("Classifying using XGBClassifier...")
 xgb_classifier = XGBClassifier()
 
-xgb_classifier.fit(X_train, y_train)
-xgb_predictions = xgb_classifier.predict(X_test)
+xgb_classifier.fit(X_train, y_train, eval_metric='auc')
+xgb_predictions = xgb_classifier.predict_proba(X_test)[:, 1]
 
 print("Computing scores...")
-xgb_accuracy_score = accuracy_score(y_test, xgb_predictions)
-xgb_auc_score = auc(y_test, xgb_predictions)
-print("Accuracy score: %s - AUC score: %s\n" % (xgb_accuracy_score, xgb_auc_score))
+xgb_auc_score = roc_auc_score(y_test, xgb_predictions, average='macro')
+print("AUC score: %s\n" % xgb_auc_score)
