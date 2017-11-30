f2a234eea09ec8ba2ef2a100f9290c6101388562
paulden <paul.de-nonancourt@student.ecp.fr>
2017-11-30 14:04:03 +0100

    Unify script to compare and deliver data

--- extra_trees_feature_selection.py
+++ extra_trees_feature_selection.py
@@ -13,11 +13,12 @@ from sklearn.feature_selection import SelectFromModel
 from time import time
 
 
+# Load data
 df = pd.read_csv('./data/train.csv')
 df_test = pd.read_csv('./data/test.csv')
 
 # Clean data
-print("Cleaning data...\n")
+print("\nCleaning data...\n")
 
 constant_columns = []
 duplicate_columns = []
@@ -34,7 +35,7 @@ for i in range(columns_length - 1):
         if np.array_equal(original_column_values, column_values_to_compare):
             duplicate_columns += [df.columns[j]]
 
-columns_to_remove = constant_columns + list(set(duplicate_columns) - set(constant_columns))
+columns_to_remove = list(set(duplicate_columns) | set(constant_columns))
 
 df.drop(columns_to_remove, axis=1, inplace=True)
 df_test.drop(columns_to_remove, axis=1, inplace=True)
@@ -45,7 +46,7 @@ print(values_df)
 
 
 # Split data between train and test
-train_df, test_df = train_test_split(df, test_size=0.2)
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=1)
 
 X_train = train_df.drop(['ID', 'TARGET'], axis=1)
 X_test = test_df.drop(['ID', 'TARGET'], axis=1)
@@ -57,8 +58,8 @@ y_test = test_df['TARGET']
 
 # Feature selection
 start_time_fs = time()
-extra_trees_classifier = ExtraTreesClassifier(random_state=1)
-selector = extra_trees_classifier.fit(X_train, y_train)
+clf = ExtraTreesClassifier(random_state=1)
+selector = clf.fit(X_train, y_train)
 
 features_selector = SelectFromModel(selector, prefit=True)
 
@@ -66,7 +67,7 @@ X_train = features_selector.transform(X_train)
 X_test = features_selector.transform(X_test)
 test = features_selector.transform(test)
 
-print("Feature selection complete: %s features selected in %.2f seconds.\n" % (X_train.shape[1], time() - start_time_fs))
+print("\nFeature selection complete: %s features selected in %.2f seconds.\n" % (X_train.shape[1], time() - start_time_fs))
 
 
 scores = {}
@@ -80,19 +81,18 @@ gb_predictions = gb_classifier.predict_proba(X_test)[:, 1]
 
 gb_auc_score = roc_auc_score(y_test, gb_predictions)
 
-scores['GradientBoostingClassifier'] = [gb_auc_score, time() - start_time_gb]
+scores['GradientBoostingClassifier'] = (gb_auc_score, time() - start_time_gb)
 
 # RandomForestClassifier
 start_time_rf = time()
-rf_classifier = RandomForestClassifier(n_estimators=50, max_depth=20, random_state=1)
+rf_classifier = RandomForestClassifier(n_estimators=100, max_depth=20, random_state=1)
 
 rf_classifier.fit(X_train, y_train)
 rf_predictions = rf_classifier.predict_proba(X_test)[:, 1]
 
 rf_auc_score = roc_auc_score(y_test, rf_predictions)
 
-scores['RandomForestClassifier'] = [rf_auc_score, time() - start_time_rf]
-
+scores['RandomForestClassifier'] = (rf_auc_score, time() - start_time_rf)
 
 # XGBClassifier
 start_time_xgb = time()
@@ -101,14 +101,34 @@ xgb_classifier = XGBClassifier()
 xgb_classifier.fit(X_train, y_train, eval_metric='auc')
 xgb_predictions = xgb_classifier.predict_proba(X_test)[:, 1]
 
-xgb_auc_score = roc_auc_score(y_test, xgb_predictions, average='macro')
+xgb_auc_score = roc_auc_score(y_test, xgb_predictions)
 
-scores['XGBoost'] = [xgb_auc_score, time() - start_time_xgb]
+scores['XGBoost'] = (xgb_auc_score, time() - start_time_xgb)
+
+
+# Find best estimator
+score_max = 0
+best_model = None
 
 for model in scores:
-    print("%s - AUC score: %.5f - Time to fit and predict: %.2f seconds." % (model, scores[model][0], scores[model][1]))
+    score = scores[model][0]
+    elapsed_time = scores[model][1]
 
-# target = xgb_classifier.predict_proba(test)[:, 1]
-#
+    print("Results for %s:" % model)
+    print("AUC score: %.5f - Time to fit and predict: %.2f seconds.\n" % (score, elapsed_time))
+    if score > score_max:
+        best_model = model
+        score_max = score
+
+print("Best model using AUC score: %s (AUC %s)" % (best_model, score_max))
+
+# Create submission with test predictions
+if best_model == 'GradientBoostingClassifier':
+    target = gb_classifier.predict_proba(test)[:, 1]
+elif best_model == 'XGBoost':
+    target = rf_classifier.predict_proba(test)[:, 1]
+elif best_model == 'RandomForestClassifier':
+    target = xgb_classifier.predict_proba(test)[:, 1]
+
 # submission = pd.DataFrame({"ID": df_test['ID'], "TARGET": target})
 # submission.to_csv("data/submission.csv", index=False)
