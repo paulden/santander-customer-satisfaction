19117fe726895fe5a5833410af2d11aafac58ff4
paulden <paul.de-nonancourt@student.ecp.fr>
2017-11-30 09:05:45 +0100

    Fix GradientBoostingClassifier issue to predict probabilities

--- analyze_data.py
+++ analyze_data.py
@@ -1,23 +1,24 @@
 import numpy as np
 import pandas as pd
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
-from sklearn.metrics import accuracy_score
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
 
 from xgboost import XGBClassifier
 
 from clean_data import *
-from select_features import *
+
+from time import time
 
 
 df = pd.read_csv('./data/train.csv')
 
-# CLEAN DATA
+print(df.shape)
+
+# Clean data
 print("Cleaning data...\n")
 df = remove_constant_columns(df)
 df = remove_duplicate_columns(df)
@@ -26,6 +27,8 @@ values_df = pd.DataFrame(df['TARGET'].value_counts())
 values_df['Percentage'] = 100 * values_df['TARGET'] / df.shape[0]
 print(values_df)
 
+
+# Split data between train and test
 train_df, test_df = train_test_split(df, test_size=0.2)
 
 X_train = train_df.drop(['ID', 'TARGET'], axis=1)
@@ -34,31 +37,43 @@ X_test = test_df.drop(['ID', 'TARGET'], axis=1)
 y_train = train_df['TARGET']
 y_test = test_df['TARGET']
 
-# Feature selection
-print("\nSelecting most important features to use using ExtraTreesClassifier")
-X_train, X_test = extra_trees(X_train, y_train, X_test)
 
-print(X_train.shape, X_test.shape)
+# Feature selection
+start_time_fs = time()
+extra_trees_classifier = ExtraTreesClassifier(random_state=1)
+selector = extra_trees_classifier.fit(X_train, y_train)
+
+features_selector = SelectFromModel(selector, prefit=True)
+
+X_train = features_selector.transform(X_train)
+X_test = features_selector.transform(X_test)
+
+print("Feature selection complete: %s features selected in %.2f seconds.\n" % (X_train.shape[1], time() - start_time_fs))
+
+
+scores = {}
 
 # GradientBoostingClassifier
-print("\nClassifying using GradientBoostingClassifier...")
+start_time_gb = time()
 gb_classifier = GradientBoostingClassifier(random_state=1)
 
 gb_classifier.fit(X_train, y_train)
-gb_predictions = gb_classifier.predict(X_test)
+gb_predictions = gb_classifier.predict_proba(X_test)[:, 1]
 
-print("Computing scores...")
-gb_accuracy_score = accuracy_score(y_test, gb_predictions)
 gb_auc_score = roc_auc_score(y_test, gb_predictions)
-print("AUC score: %s\n" % gb_auc_score)
+
+scores['GradientBoostingClassifier'] = [gb_auc_score, time() - start_time_gb]
 
 # XGBClassifier
-print("Classifying using XGBClassifier...")
+start_time_xgb = time()
 xgb_classifier = XGBClassifier()
 
 xgb_classifier.fit(X_train, y_train, eval_metric='auc')
 xgb_predictions = xgb_classifier.predict_proba(X_test)[:, 1]
 
-print("Computing scores...")
 xgb_auc_score = roc_auc_score(y_test, xgb_predictions, average='macro')
-print("AUC score: %s\n" % xgb_auc_score)
+
+scores['XGBoost'] = [xgb_auc_score, time() - start_time_xgb]
+
+for model in scores:
+    print("%s - AUC score: %.5f - Time to fit and predict: %.2f seconds." % (model, scores[model][0], scores[model][1]))
